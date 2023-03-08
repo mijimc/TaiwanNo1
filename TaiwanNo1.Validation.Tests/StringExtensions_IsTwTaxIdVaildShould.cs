7a5499f673421337c30c98a9aff2516675f06dd4
張庭瑀 Barney <barney@miniasp.com>
2023-03-08 15:01:06 +0800

    (feat) 新增台灣統一編號驗證(新式)

---
+++ TaiwanNo1.Validation.Tests/StringExtensions_IsTwTaxIdVaildShould.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace TaiwanNo1.Validation.Tests
+{
+    public class StringExtensions_IsTwTaxIdVaildShould
+    {
+        [Theory]
+        [InlineData("22099131")]
+        [InlineData("16003518")]
+        [InlineData("96979933")]
+        [InlineData("20828393")]
+        [InlineData("23638777")]
+        [InlineData("12694272")]
+        public void IsTwTaxIdVaild_InputDifferentTaxIds_ReturnTrue(string taxId)
+        {
+            taxId.IsTwTaxIdVaild().Should().BeTrue();
+        }
+    }
+}
